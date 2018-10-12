8822ea8bf7c2a894beefc30179c08a1c66af9c81
Ethan Berman <bermane@noyce.whitman.edu>
2018-10-12 10:25:38 -0700

    starter files

---
+++ sequence3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "sequence3.h"
+using namespace std;
+namespace main_savitch_5{
+	sequence::sequence(){
+	}
+	//copy constructor
+	sequence::sequence(const sequence& source){
+
+	}
+}
+int main(){
+
+	return 0;
+}
+
