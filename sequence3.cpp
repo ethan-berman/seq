bb7ddd9be337d4480f6e97a36b77ce80a32c1ea0
Ethan Berman <bermane@noyce.whitman.edu>
2018-10-12 10:51:24 -0700

    asd

--- sequence3.cpp
+++ sequence3.cpp
@@ -2,12 +2,39 @@
 #include "sequence3.h"
 using namespace std;
 namespace main_savitch_5{
+	//default assignment constructor
 	sequence::sequence(){
+	
 	}
 	//copy constructor
 	sequence::sequence(const sequence& source){
 
 	}
+	//destructor
+	sequence::~sequence(){
+	}
+	//modification member functions
+	void sequence::start(){
+	
+	}
+	void sequence::advance(){
+
+	}
+	void sequence::insert(const value_type& entry){
+
+	}
+	void sequence::attach(const value_type& entry){
+
+	}
+	void sequence::operator =(const sequence& source){
+	}
+	void sequence::remove_current(){
+	}
+	//const member functions
+	value_type sequence::current() const{
+	}
+
+
 }
 int main(){
 
